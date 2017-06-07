13f6284127367b8364adcced5c50023ff4d9f518
Shubham16598 <shubham16598@gmail.com>
2017-06-07 16:40:16 +0530

    Custom Adapter Added

---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,23 @@
+package com.example.android.miwok;
+
+/**
+ * Created by shubham16598 on 7/6/17.
+ */
+
+public class Word {
+    private String mDefaultTranslation;
+    private String mMiwokTranslation;
+
+    public Word(String mDefaultTranslation, String mMiwokTranslation) {
+        this.mDefaultTranslation = mDefaultTranslation;
+        this.mMiwokTranslation = mMiwokTranslation;
+    }
+
+    public String getmDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    public String getmMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+}
