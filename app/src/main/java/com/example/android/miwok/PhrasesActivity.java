9dde5f138c9e9d41253bd185307b928dfaeafdae
Shubham16598 <shubham16598@gmail.com>
2017-06-07 16:56:39 +0530

    Java Code Updated

--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -17,6 +17,9 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class PhrasesActivity extends AppCompatActivity {
 
@@ -24,5 +27,22 @@ public class PhrasesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phrases);
+        ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("one","lutti"));
+        words.add(new Word("two","otiiko"));
+        words.add(new Word("three","tolookosu"));
+        words.add(new Word("four","olisa"));
+        words.add(new Word("five","temokka"));
+        words.add(new Word("six","nicha"));
+        words.add(new Word("seven","tilikotuu"));
+        words.add(new Word("eight","forula"));
+        words.add(new Word("nine","olyyaisa"));
+        words.add(new Word("ten","vilisu"));
+
+        WordAdapter adapter = new WordAdapter(this, words);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(adapter);
     }
 }
