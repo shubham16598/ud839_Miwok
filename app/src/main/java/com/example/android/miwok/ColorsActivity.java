d832ab5e84cd379d51d8c6f720f3226c578c8937
Shubham16598 <shubham16598@gmail.com>
2017-06-07 17:05:16 +0530

    Final Custom layout code completed

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -26,7 +26,7 @@ public class ColorsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_colors);
+        setContentView(R.layout.words_items);
         ArrayList<Word> words = new ArrayList<Word>();
         words.add(new Word("one","lutti"));
         words.add(new Word("two","otiiko"));
--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -26,7 +26,7 @@ public class PhrasesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phrases);
+        setContentView(R.layout.words_items);
         ArrayList<Word> words = new ArrayList<Word>();
         words.add(new Word("one","lutti"));
         words.add(new Word("two","otiiko"));
